2355456f80644536728502be79b8402c27aa890b
Blake Miller <blak3mill3r@gmail.com>
2011-06-19 21:32:53 -0700

    Initial commit to ractionscript.

---
+++ test/test_ractionscript.rb
@@ -0,0 +1,7 @@
+require 'helper'
+
+class TestRactionscript < Test::Unit::TestCase
+  should "probably rename this file and start testing for real" do
+    flunk "hey buddy, you should probably rename this file and start testing for real"
+  end
+end
