8e5881e5832c46f32e38c455a0858e56f347a7fe
bmiller <bmiller@handson.com>
2011-07-02 22:04:28 +0000

    Cleanup, preparing to transition from toy to tool

--- lib/ractionscript.rb
+++ lib/ractionscript.rb
@@ -1,6 +1,9 @@
 # add current dir to ruby load path
 $:.unshift File.dirname(__FILE__)
 
+# load rake tasks
+Dir["tasks/**/*.rake"].each { |ext| load ext } if defined?(Rake)
+
 # dependencies
 require 'rjb'
 require 'ruby2ruby'
@@ -21,89 +24,13 @@ require 'ractionscript/rjb'
 Ractionscript::Rjb.load_metaas # must Rjb-import the metaas dependencies before 'constants' is loaded below
 require 'ractionscript/constants'
 require 'ractionscript/dsl/builder_context' # this uses Rjb so must be required after 'load_metaas'
-
-# NOTE
-# currently all this does is run your ractionscript source's ruby-syntax s-expression
-# through ractionscript's translators which should decide
-# what's ruby and what's actionscript-generating-dsl
-# then passes that through the generators
-# which produces ruby code calling metaas through Rjb to produce ActionScript source
+require 'ractionscript/dsl'
 
 # load a simple ractionscript source file sample and get it's ruby-syntax s-expression
-
 ractionscript_source_file = File.join( File.dirname(__FILE__), 'ractionscript_simpletest.as3.rb' )
+ractionscript_source      = File.read( ractionscript_source_file )
 
-ractionscript_source = File.read( ractionscript_source_file )
-
-sexp = Ractionscript::Sexp.ruby_string_to_sexp( ractionscript_source )
-
-# for development, display it in graphviz and highlight a matching subexpression
-system "killall rsvg-view"  # temp, convenient for me
-
-highlight = Q?{
-  s(:call, nil, :args!, _ % :paramlist) % :highlight
-}
-
-# translate the ruby-syntax s-expression
-# rewriting the parts that are meant to become actionscript
-# leaving the rest untouched
-# start from the highest level scope of ractionscript syntax (class definition)
-# work down the scope rules
-# reason for splitting translators into many classes is twofold
-# 1) separation of concerns, potentially help make this a part of a generic library ruby-babel
-# 2) SexpBuilder is not recursive, i.e. it wont rewrite s-expressions within rewritten ones
-# so it is a chain of SexpBuilders (translators)
-
-# to visualize it anywhere along the way insert this between calls to translators or generators 'process'
-#Ractionscript::Sexp.viz_sexp(sexp, highlight); exit(0)
-
-sexp = Ractionscript::DSL::Translators::ClassDefinition.new.process(sexp)
-
-sexp = Ractionscript::DSL::Translators::MethodDefinition.new.process(sexp)
-
-sexp = Ractionscript::DSL::Translators::Expression.new.process(sexp)
-
-# now generators...
-# at this point 'sexp' is not a valid ruby AST, it has a bunch of ractionscript specific things
-# the generators convert this to a ruby AST
-# which will build ActionScript AST with metaas Java classes
-
-sexp = Ractionscript::DSL::Generators::CompilationUnit.new.process(sexp)
-
-sexp = Ractionscript::DSL::Generators::MethodDefinition.new.process(sexp)
-
-sexp = Ractionscript::DSL::Generators::Expression.new.process(sexp)
-
-# back to ruby source string
-sourcebuilder = Ruby2Ruby.new.process( sexp )
-
-#puts "--------------------source builder--------------"
-#puts sourcebuilder
-#puts "--------------------source builder--------------"
-
-# careful with class_eval, it'd better not litter or we better reload the class every time
-#load 'ractionscript/builder_context'
-
-Ractionscript::DSL::BuilderContext.class_eval(sourcebuilder)
-
-# and now...
-
-bc = Ractionscript::DSL::BuilderContext.new
-
-compilation_unit = bc.metacompile
-
-# 'metacompile' has returned an Rjb instance of the metaas compilation-unit class
-# we need a Java StringWriter instance to make 'compilation_unit' do it's thing
-
-sw = Ractionscript::JavaTypes::Util::StringWriter.new
-writer = Ractionscript::AST::Factory.newWriter
-writer._invoke('write',
-               'Ljava.io.Writer;Luk.co.badgersinfoil.metaas.dom.ASCompilationUnit;',
-               sw,
-               compilation_unit
-              )
-
-as3_source_code = sw.toString
+as3_source_code = Ractionscript::DSL.process( ractionscript_source )
 
 puts "--------------------ActionScript--------------"
 puts as3_source_code
---
+++ lib/ractionscript/dsl.rb
@@ -0,0 +1,79 @@
+module Ractionscript
+
+  module DSL
+    def DSL.process(ractionscript_source)
+
+      sexp = Ractionscript::Sexp.ruby_string_to_sexp( ractionscript_source )
+      
+      # for development, display it in graphviz and highlight a matching subexpression
+      system "killall rsvg-view"  # temp, convenient for me
+      
+      highlight = Q?{
+        s(:call, nil, :args!, _ % :paramlist) % :highlight
+      }
+      
+      # translate the ruby-syntax s-expression
+      # rewriting the parts that are meant to become actionscript
+      # leaving the rest untouched
+      # start from the highest level scope of ractionscript syntax (class definition)
+      # motivation for splitting translators into many classes is:
+      # 1) separation of concerns, potentially help make this a part of a generic library ruby-babel
+      # 2) SexpBuilder is not recursive, i.e. it wont rewrite s-expressions within rewritten ones
+      # so it is a chain of SexpBuilders (translators)
+      
+      # to visualize it anywhere along the way insert this between calls to translators or generators 'process'
+      #Ractionscript::Sexp.viz_sexp(sexp, highlight); exit(0)
+      
+      sexp = Translators::ClassDefinition.new.process(sexp)
+      
+      sexp = Translators::MethodDefinition.new.process(sexp)
+      
+      sexp = Translators::Expression.new.process(sexp)
+      
+      # now generators...
+      # at this point 'sexp' is not a valid ruby AST, it has a bunch of ractionscript specific things
+      # the generators convert this to a ruby AST
+      # which will build ActionScript AST with metaas Java classes
+      
+      sexp = Generators::CompilationUnit.new.process(sexp)
+      
+      sexp = Generators::MethodDefinition.new.process(sexp)
+      
+      sexp = Generators::Expression.new.process(sexp)
+      
+      # back to ruby source string
+      sourcebuilder = Ruby2Ruby.new.process( sexp )
+      
+      puts "--------------------source builder--------------"
+      puts sourcebuilder
+      puts "--------------------source builder--------------"
+      
+      # careful with class_eval, it'd better not litter or we better reload the class every time
+      #load 'ractionscript/builder_context'
+      
+      BuilderContext.class_eval(sourcebuilder)
+      
+      # and now...
+      
+      context = BuilderContext.new
+      
+      compilation_unit = context.metacompile
+      
+      # 'metacompile' has returned an Rjb instance of the metaas compilation-unit class
+      # we need a Java StringWriter instance to make 'compilation_unit' do it's thing
+      
+      string_writer = Ractionscript::JavaTypes::Util::StringWriter.new
+      writer = Ractionscript::AST::Factory.newWriter
+      writer._invoke('write',
+                     'Ljava.io.Writer;Luk.co.badgersinfoil.metaas.dom.ASCompilationUnit;',
+                     string_writer,
+                     compilation_unit
+                    )
+      
+      return string_writer.toString
+
+    end
+  end
+
+end
+
